0a22943304a80e1c67b0811ec2a6e0d32461a286
Allwin <allwinselva7@gmail.com>
2025-09-10 14:58:52 +0530

    Deleted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
-import Authentication from "./ConditionalRendering/Authentication";
-import Weather from "./ConditionalRendering/Weather";
-import UpdateCareer from "./ContextApi/UpdateCareer";
-import FetchDataEffect from "./Hooks/UseEffectd/FetchDataEffect";
-import FocusInput from "./Hooks/UseRef/FocusInput";
-import Counter from "./Hooks/UseState/Counter";
-import ShoppingCart from "./Hooks/UseState/ShoppingCart";
-import Map from "./Methods/Map";
-import Person from "./Props/Person";
-import UpdatingArrObject from "./Hooks/UseState/upObject";
-
 function App() {
-  return (
-    <div>
-      {/* <Map />
-      <Person name="thePrimeTime" age={52} career="Youtuber" />{" "} */}
-      {/*-have to pass number inside curly braces - */}
-      {/* <Weather temperature={25} />
-      <Authentication isAdmin={true} isLoggedIn={true} />
-      <Counter />
-      <ShoppingCart />
-      <FetchDataEffect />
-      <UpdateCareer />
-      <FocusInput /> */}
-      <UpdatingArrObject />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
